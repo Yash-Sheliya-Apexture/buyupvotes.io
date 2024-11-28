2bbfc119a2386ff1325c8eb669d898d1c91afcbb
deny2410 <rudrasutariya003@gmail.com>
2024-11-28 15:32:52 +0530

    remove Hero-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "./Components/Header";
-import HeroSection from "./Components/HeroSection";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <HeroSection />
     </div>
   );
 };
