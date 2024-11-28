b5aac2d30c7aeb49ccc68ba4352e0a4bf7dda086
deny2410 <rudrasutariya003@gmail.com>
2024-11-28 15:31:13 +0530

    Add Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 };
