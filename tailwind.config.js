12f20f5395991f730bddcdbfb9bfa469965ca8e9
deny2410 <rudrasutariya003@gmail.com>
2024-11-28 11:40:52 +0530

    project Set-Up Add Font-Family

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        LatoFont: ["Lato", "serif"],
+      },
+      screens: {
+        xs: { min: "320px" },
+      },
+    },
+  },
+  plugins: [],
+};
